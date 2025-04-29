141482af01131f3bb8e8c647f26657d78a643b59
zt <52065229+zengten@users.noreply.github.com>
2025-04-29 03:24:31 +0000

    :sparkles: ref属性和props配置项使用

---
+++ vue-advance/03_src_props_use/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Student name="李四" sex="男"></Student>
+    <hr>
+    <!-- 当你在模板中直接写 age="18" 时，Vue 会将其视为字符串类型的值。而使用 v-bind 指令，可以让 Vue 对绑定的值进行 JavaScript 表达式的求值 -->
+    <Student name="李四" :age="18" sex="男"></Student>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: '欢迎学习Vue'
+    }
+  },
+  components: {Student}
+}
+
+</script>
---
+++ vue-advance/03_src_props_use/components/Student.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2 @click="showName">学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <h2>学生年龄：{{ age + 1 }}</h2>
+    <h2>学生心理年龄: {{ myAge }}</h2>
+    <button @click="changeAge">点我修改年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      myAge: this.age + 20
+    }
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+    changeAge() {
+      // 使用props配置的属性，不允许再修改，除非使用变量中转
+      // this.age++;
+      this.myAge++;
+    },
+  },
+  // props简单写法
+  // props: ["name", "age", "sex"],
+  // 限制props中属性的类型 类型错误了会提示错误信息
+  // props: {
+  //   name: String,
+  //   age: Number,
+  //   sex: String,
+  // },
+  // 接收时不仅限制类型还加上默认值的指定并指定它的必须性
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+    age: {
+      required: false,
+      type: Number,
+      default: 99,
+    },
+    sex: String,
+  },
+};
+</script>
