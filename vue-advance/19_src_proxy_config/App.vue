dd33a8444dafe7868540478a1f691b2be03f850a
zt <52065229+zengten@users.noreply.github.com>
2025-05-23 11:12:39 +0000

    :sparkles: 配置请求代理服务器

---
+++ vue-advance/19_src_proxy_config/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="getStudentList">点击获取学生信息</button>
+    <button @click="getCarList">点击获取汽车信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: 'App',
+  methods: {
+    getStudentList() {
+      axios.get('http://localhost:8889/api1/students').then(resp => {
+        console.log(resp.data)
+      }, error => {
+        console.log(error.message)
+      })
+    },
+    getCarList() {
+      axios.get('http://localhost:8889/api2/cars').then(resp => {
+        console.log(resp.data)
+      }, error => {
+        console.log(error.message)
+      })
+    }
+  }
+}
+
+</script>
+
