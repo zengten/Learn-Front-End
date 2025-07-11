53321c686e03ff2611c70c52bece9238a5904802
zt <52065229+zengten@users.noreply.github.com>
2025-07-11 06:35:09 +0000

    :sparkles: vue-router中的props用法

---
+++ vue-advance/src/pages/News.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>news01</ul>
+    <ul>news02</ul>
+    <ul>news03</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  beforeDestroy() {
+    console.log('news beforeDestroy')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
