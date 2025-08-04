a24fd1b19647323ec07fa441918d1bc1cdcc56e3
zt <52065229+zengten@users.noreply.github.com>
2025-08-04 03:27:50 +0000

    :sparkles: vue-router生命周期函数

---
+++ vue-advance/39_src_router_lifecycle_func/pages/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li>消息id: {{ id }}</li>
+      <li>消息标题：{{ title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: ["id", "title"],
+  mounted() {
+    // route里面会有query参数，取出来即可展示
+    console.log("detail route @", this.$route);
+    console.log("detail router @", this.$router);
+  },
+};
+</script>
+
+<style>
+</style>
