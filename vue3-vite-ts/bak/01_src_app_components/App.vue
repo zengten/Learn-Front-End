b1672eff4d0876e5c3fd4f4e9c683f4b99dceee0
zt <52065229+zengten@users.noreply.github.com>
2025-08-25 11:21:30 +0000

    :tada: 初始化vue3脚手架

---
+++ vue3-vite-ts/bak/01_src_app_components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>你好啊</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
