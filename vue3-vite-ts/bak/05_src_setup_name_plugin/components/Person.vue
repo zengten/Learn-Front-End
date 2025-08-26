7b43cd83deeceb9065616f05e35da34ae6788a93
zt <52065229+zengten@users.noreply.github.com>
2025-08-26 12:39:48 +0000

    :sparkles: setup使用插件简写

---
+++ vue3-vite-ts/bak/05_src_setup_name_plugin/components/Person.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="showMobile">点击展示手机号</button>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+  </div>
+</template>
+
+<!-- 使用name属性需要插件支持-->
+<!-- npm i vite-plugin-vue-setup-extend -D -->
+<!-- 并修改vite.config.ts-->
+<script lang="ts" setup name="Person111">
+let name = '张三'
+let age = 20
+let mobile = '18112344321'
+let showMobile = () => alert(mobile)
+let changeName = () => {
+  name = 'zhang-san'
+  console.log(name)
+}
+let changeAge = () => {
+  age++
+  console.log(age)
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
